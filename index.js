d3e69627c847cb9ed0b039ece6da0bc8a0546229
Alvaro <alvarodromantligero@alumnos.tuniversformacion.es>
2023-10-03 10:49:13 +0200

    Creacion de identificacion de numeros

---
+++ index.js
@@ -0,0 +1,24 @@
+let numero = prompt("Por favor, ingresa un número:");
+
+
+ let num = parseInt(numero);
+
+if (num > 0) {
+   
+ console.log("El número ingresado es positivo.");
+alert("El número ingresado es positivo.");
+} else if (num < 0) {
+  
+console.log("El número ingresado es negativo.");
+alert("El número ingresado es negativo.");
+} else {
+
+console.log("El número ingresado es igual a cero.");
+alert("El número ingresado es igual a cero.");
+}
+
+
+
+
+
+
